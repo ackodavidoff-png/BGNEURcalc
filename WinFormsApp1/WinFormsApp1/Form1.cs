c300a40ab6a2c23ac8a6146e4c06ae1558695f09
Александър Давидов <ackodavidoff@gmail.com>
2026-01-19 17:13:22 +0200

    Initial commit

---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,56 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //1 EUR = 1.95583 BGN
+            double course = 1.95583;
+            double amount;
+            double result;
+            if (double.TryParse(textBox1.Text, out amount))
+            {
+                if (radioButton1.Checked)
+                {
+                    if (amount >= 2000)
+                    {
+                        MessageBox.Show($"Има лимит за превалутиране.Трябва да декларирате парите,които имате: {amount:F2} Лева", "Предупреждение");
+                    }
+                    result = amount / course;
+                    label2.Text = $"{amount:F2} BGN = {result:F2} EUR";
+                }
+                else if (radioButton2.Checked)
+                {
+                    result = amount * course;
+                    label2.Text = $"{amount:F2} EUR = {result:F2} BGN";
+                }
+                else
+                {
+                    MessageBox.Show("Моля изберете опция");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Невалиден вход.Моля опитайте отново!", "Предупреждение");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            if (radioButton1.Checked)
+            {
+                label2.Text = "0.00 BGN = 0.00 EUR";
+            }
+            else if (radioButton2.Checked)
+            {
+                label2.Text = "0.00 EUR = 0.00 BGN";
+            }
+        }
+    }
+}
